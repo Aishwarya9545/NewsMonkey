9d24a32fc69148895b529e26a5efde7a3d3f0416
Aishwarya Babaji Shelke <aishwarya.shelke@Yash.com>
2022-11-15 12:48:03 +0530

    this is 1st commit

---
+++ newsapp/src (6)/src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+    let {title, discription,imageUrl,newsUrl}=this.props
+    return (
+        
+                    <div className="my-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src={imageUrl}
+                         className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{discription}</p>
+                    <a href={ newsUrl} className="btn btn-sn btn-primary">ReadMore</a>
+                </div>
+                </div>
+                    </div>  
+
+    )
+  }
+}
+
+export default NewsItem
